292aa7813e39f3cfbc4cc8bfa37cb17c24d8c47c
GabiP13 <64442012+GabiP13@users.noreply.github.com>
2025-05-03 23:49:18 -0700

    checking with alert(1)

--- background.js
+++ background.js
@@ -4,16 +4,12 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
 
-
-const extensions = 'https://developer.chrome.com/docs/extensions'
-const webstore = 'https://developer.chrome.com/docs/webstore'
-  
-
-
+// const extensions = 'https://developer.chrome.com/docs/extensions'
+// const webstore = 'https://developer.chrome.com/docs/webstore'
 
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (true) {//tab.url.startsWith(extensions) || tab.url.startsWith(webstore)
+  if (1) {//tab.url.startsWith(extensions) || tab.url.startsWith(webstore)
     // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     // Next state will always be the opposite
